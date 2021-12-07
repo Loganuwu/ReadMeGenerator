2eb307ef1764b93b7d5bc6f74096523c16603624
Loganuwu <53189601+Loganuwu@users.noreply.github.com>
2021-12-06 20:14:54 -0800

    updated to contact info

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,7 @@ function generateMarkdown(data, licenseList) {
   - [License](#License)
   - [Contributing](#Contributing)
   - [Tests](#Test-Instructions)
-  - [Questions](#Questions)
+  - [Contact](#Contact)
   ## Installation
   ${data.install_instructions}
   ## Usage
@@ -24,7 +24,7 @@ function generateMarkdown(data, licenseList) {
   ${data.contribution}
   ## Tests
   ${data.test_instructions}
-  ## Questions
+  ## Contact
   - Github is [${data.github_username}](https://github.com/${data.github_username})
   - Email is [${data.email}](${data.email})
   `;
